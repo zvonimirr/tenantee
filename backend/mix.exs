f6db3bcef010dce2eb46be44c11edde617fcaa28
Zvonimir Rudinski <zvonimirurdinski@protonmail.ch>
2022-11-19 03:28:24 +0100

    feat: add credo

--- backend/mix.exs
+++ backend/mix.exs
@@ -42,7 +42,8 @@ defmodule Tenantee.MixProject do
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
       {:money, "~> 1.12"},
-      {:cors_plug, "~> 3.0"}
+      {:cors_plug, "~> 3.0"},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
     ]
   end
 
