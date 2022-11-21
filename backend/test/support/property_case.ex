919ae66fad9621001a4fd7931f93ff1a6c09bbb7
Zvonimir Rudinski <zvonimirurdinski@protonmail.ch>
2022-11-21 09:27:01 +0100

    test: finish property controller test

---
+++ backend/test/support/property_case.ex
@@ -0,0 +1,26 @@
+defmodule TenanteeWeb.PropertyCase do
+  use ExUnit.CaseTemplate
+
+  using do
+    quote do
+      defp insert_property(conn) do
+        post conn, "/api/properties", %{
+          property: %{
+            name: "Test Property",
+            location: "Test Location",
+            description: "Test Description",
+            price: 100
+          }
+        }
+      end
+    end
+  end
+
+  setup_all do
+    :ok
+  end
+
+  setup do
+    :ok
+  end
+end
