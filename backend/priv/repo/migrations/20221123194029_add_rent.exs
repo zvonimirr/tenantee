229a59cb0a5eaf1041e63cb6a564d1b1acd9b08c
Zvonimir Rudinski <zvonimirurdinski@protonmail.ch>
2022-11-23 20:51:11 +0100

    fix: fix defaults in migration

--- backend/priv/repo/migrations/20221123194029_add_rent.exs
+++ backend/priv/repo/migrations/20221123194029_add_rent.exs
@@ -3,8 +3,8 @@ defmodule Tenantee.Repo.Migrations.AddRent do
 
   def up do
     create table(:rent) do
-      add :due_date, :date
-      add :paid, :boolean
+      add :due_date, :date, default: fragment("now()")
+      add :paid, :boolean, default: false
       add :tenant_id, references(:tenants, on_delete: :delete_all)
       add :property_id, references(:properties, on_delete: :delete_all)
 
