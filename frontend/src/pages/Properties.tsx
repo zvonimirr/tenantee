f33a2e85074276a7a2f80a70f8791b027142fd9f
Zvonimir Rudinski <zvonimirurdinski@protonmail.ch>
2022-11-23 13:36:03 +0100

    feat: allow property update from the ui

--- frontend/src/pages/Properties.tsx
+++ frontend/src/pages/Properties.tsx
@@ -16,14 +16,20 @@ import {
     propertyApiService,
     PropertyApiService,
 } from '../services/api/PropertyApiService';
-import { Property, PropertyDto, PropertyList } from '../types/property';
+import {
+    Property,
+    PropertyDto,
+    PropertyList,
+    PropertyUpdateDto,
+} from '../types/property';
 import PropertyCard from '../components/Property/PropertyCard';
 import { useNavigate } from 'react-router-dom';
 import { useCallback, useEffect, useMemo, useState } from 'react';
 import AddPropertyModal from '../components/Property/Modals/AddPropertyModal';
 import ConfirmModal from '../components/Modals/ConfirmModal';
 import { useNotification } from '../hooks/useNotification';
-import { isEmpty, T } from 'ramda';
+import { isEmpty } from 'ramda';
+import EditPropertyModal from '../components/Property/Modals/EditPropertyModal';
 
 function Properties() {
     const { data, error, isValidating, mutate } = useSWR<PropertyList>(
@@ -31,13 +37,18 @@ function Properties() {
         propertyApiService.getProperties,
     );
     const navigate = useNavigate();
-    const [propertyToDelete, setPropertyToDelete] = useState<Property | null>(
+    const [selectedProperty, setSelectedProperty] = useState<Property | null>(
         null,
     );
     const {
         isOpen: isAddNewPropertyModalOpen,
         onOpen: openAddNewPropertyModal,
         onClose: closeAddNewPropertyModal,
+    } = useDisclosure();
+    const {
+        isOpen: isEditPropertyModalOpen,
+        onOpen: openEditPropertyModal,
+        onClose: closeEditPropertyModal,
     } = useDisclosure();
     const {
         isOpen: isConfirmModalOpen,
@@ -53,7 +64,12 @@ function Properties() {
 
     const isError = useMemo(() => error !== undefined, [error]);
 
-    const onSubmit = useCallback(
+    const onPropertyCardClick = useCallback(
+        (property: Property) => navigate(`/properties/${property.id}`),
+        [navigate],
+    );
+
+    const onAddPropertySubmit = useCallback(
         async (property: PropertyDto) => {
             try {
                 await propertyApiService.addNewProperty(
@@ -78,34 +94,55 @@ function Properties() {
         [mutate],
     );
 
-    const onPropertyCardClick = useCallback(
-        (property: Property) => navigate(`/properties/${property.id}`),
-        [navigate],
-    );
-
-    const onPropertyDeleteClick = useCallback(async () => {
-        if (propertyToDelete) {
+    const onEditPropertySubmit = useCallback(
+        async (property: PropertyUpdateDto) => {
             try {
-                await propertyApiService.deleteProperty(
-                    PropertyApiService.deletePropertyPath(propertyToDelete.id),
+                await propertyApiService.updateProperty(
+                    PropertyApiService.updatePropertyPath(property.id),
+                    property,
                 );
 
                 showSuccess(
-                    'Property deleted',
-                    `${propertyToDelete.name} has been deleted successfully`,
+                    'Property updated',
+                    `Property ${property.property.name} has been updated successfully`,
                 );
             } catch (e) {
                 showError(
                     'Error',
-                    `An error occurred while trying to delete ${propertyToDelete.name}`,
+                    `An error occurred while trying to update ${selectedProperty?.name} property`,
                 );
             } finally {
-                setPropertyToDelete(null);
+                setSelectedProperty(null);
+                closeEditPropertyModal();
+                mutate();
+            }
+        },
+        [mutate, selectedProperty],
+    );
+
+    const onPropertyDeleteClick = useCallback(async () => {
+        if (selectedProperty) {
+            try {
+                await propertyApiService.deleteProperty(
+                    PropertyApiService.deletePropertyPath(selectedProperty.id),
+                );
+
+                showSuccess(
+                    'Property deleted',
+                    `${selectedProperty.name} has been deleted successfully`,
+                );
+            } catch (e) {
+                showError(
+                    'Error',
+                    `An error occurred while trying to delete ${selectedProperty.name}`,
+                );
+            } finally {
+                setSelectedProperty(null);
                 mutate();
                 closeConfirmModal();
             }
         }
-    }, [mutate, propertyToDelete]);
+    }, [mutate, selectedProperty]);
 
     useEffect(() => {
         if (isError) {
@@ -121,14 +158,20 @@ function Properties() {
             <ConfirmModal
                 isOpen={isConfirmModalOpen}
                 title="Delete Property"
-                message={`Are you sure you want to delete ${propertyToDelete?.name}?`}
+                message={`Are you sure you want to delete ${selectedProperty?.name}?`}
                 onConfirm={onPropertyDeleteClick}
                 onCancel={closeConfirmModal}
             />
             <AddPropertyModal
                 isOpen={isAddNewPropertyModalOpen}
                 onClose={closeAddNewPropertyModal}
-                onSubmit={onSubmit}
+                onSubmit={onAddPropertySubmit}
+            />
+            <EditPropertyModal
+                isOpen={isEditPropertyModalOpen}
+                onClose={closeEditPropertyModal}
+                onSubmit={onEditPropertySubmit}
+                property={selectedProperty}
             />
             <Breadcrumbs
                 items={[{ href: '/properties', label: 'Properties' }]}
@@ -153,8 +196,12 @@ function Properties() {
                                         property={property}
                                         onClick={onPropertyCardClick}
                                         onDeleteClick={(property) => {
-                                            setPropertyToDelete(property);
+                                            setSelectedProperty(property);
                                             openConfirmModal();
+                                        }}
+                                        onEditClick={(property) => {
+                                            setSelectedProperty(property);
+                                            openEditPropertyModal();
                                         }}
                                     />
                                 </GridItem>
