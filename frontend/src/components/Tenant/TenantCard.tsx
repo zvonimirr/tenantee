3fe9905e8e27791ddf8b199fe934bd6b7e530c7a
Andrej Kovacic <andrej.kovacic@noubis.com>
2022-11-23 17:53:37 +0100

    Add pencil icon to tenant card

--- frontend/src/components/Tenant/TenantCard.tsx
+++ frontend/src/components/Tenant/TenantCard.tsx
@@ -1,21 +1,28 @@
 import { Card, CardBody, Center, Flex, Stack, Text } from '@chakra-ui/react';
-import { IconTrash, IconUser } from '@tabler/icons';
+import { IconPencil, IconTrash, IconUser } from '@tabler/icons';
 import { Tenant } from '../../types/tenant';
 
 interface TenantCardProps {
     tenant: Tenant;
     onDeleteClick: (tenant: Tenant) => void;
+    onEditClick: (tenant: Tenant) => void;
 }
 
-function TenantCard({ tenant, onDeleteClick }: TenantCardProps) {
+function TenantCard({ tenant, onDeleteClick, onEditClick }: TenantCardProps) {
     return (
         <Card>
             <CardBody>
-                <IconTrash
-                    color="red"
-                    cursor="pointer"
-                    onClick={() => onDeleteClick(tenant)}
-                />
+                <Flex gap={2}>
+                    <IconTrash
+                        color="red"
+                        cursor="pointer"
+                        onClick={() => onDeleteClick(tenant)}
+                    />
+                    <IconPencil
+                        cursor="pointer"
+                        onClick={() => onEditClick(tenant)}
+                    />
+                </Flex>
                 <Center>
                     <Flex direction="column">
                         <IconUser size={128} />
