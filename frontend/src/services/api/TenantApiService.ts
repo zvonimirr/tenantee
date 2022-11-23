088d34c61fce555d32a55f280a20ad3de2dc670d
Andrej Kovacic <andrej.kovacic@noubis.com>
2022-11-23 17:48:15 +0100

    Add update tenant patch request function to api service

--- frontend/src/services/api/TenantApiService.ts
+++ frontend/src/services/api/TenantApiService.ts
@@ -4,6 +4,7 @@ import { HttpService } from './HttpService';
 export class TenantApiService {
     public static readonly listTenantsPath = () => '/api/tenants';
     public static readonly addTenantPath = () => '/api/tenants';
+    public static readonly updateTenantPath = (id: number) => `/api/tenants/${id}`; 
     public static readonly deleteTenantPath = (id: number) =>
         `/api/tenants/${id}`;
 
@@ -13,6 +14,10 @@ export class TenantApiService {
 
     async addTenant(path: string, tenant: TenantDto) {
         return HttpService.post<TenantDto>(path, tenant);
+    }
+
+    async updateTenant(path: string, tenant: TenantDto) {
+        return HttpService.patch<TenantDto>(path, tenant);
     }
 
     async deleteTenant(path: string) {
