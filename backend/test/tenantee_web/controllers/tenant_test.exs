14ef7f7710f10154f6478c2c9b8d24073adcb4fd
Zvonimir Rudinski <zvonimirurdinski@protonmail.ch>
2022-11-27 06:25:49 +0100

    fix: fix tenant controller test

--- backend/test/tenantee_web/controllers/tenant_test.exs
+++ backend/test/tenantee_web/controllers/tenant_test.exs
@@ -8,17 +8,17 @@ defmodule TenanteeWeb.TenantControllerTest do
     conn = insert_tenant(conn)
     failure_conn = post(conn, "/api/tenants")
 
-    assert json_response(conn, 201)["tenant"]["id"] != nil
+    assert json_response(conn, 201)["id"] != nil
     assert json_response(failure_conn, 400) == %{"error" => "Invalid params"}
   end
 
   test "GET /api/tenants/:id", %{conn: conn} do
     conn = insert_tenant(conn)
-    id = json_response(conn, 201)["tenant"]["id"]
+    id = json_response(conn, 201)["id"]
 
     conn = get(conn, "/api/tenants/#{id}")
 
-    assert json_response(conn, 200)["tenant"]["id"] == id
+    assert json_response(conn, 200)["id"] == id
   end
 
   test "GET /api/tenants", %{conn: conn} do
@@ -31,7 +31,7 @@ defmodule TenanteeWeb.TenantControllerTest do
 
   test "PATCH /api/tenants/:id", %{conn: conn} do
     conn = insert_tenant(conn)
-    id = json_response(conn, 201)["tenant"]["id"]
+    id = json_response(conn, 201)["id"]
 
     conn =
       patch(conn, "/api/tenants/#{id}", %{
@@ -42,13 +42,13 @@ defmodule TenanteeWeb.TenantControllerTest do
 
     failure_conn = patch(conn, "/api/tenants/#{id}", %{})
 
-    assert json_response(conn, 200)["tenant"]["name"] == "Updated Tenant"
+    assert json_response(conn, 200)["name"] == "Updated Tenant"
     assert json_response(failure_conn, 400) == %{"error" => "Invalid params"}
   end
 
   test "DELETE /api/tenants/:id", %{conn: conn} do
     conn = insert_tenant(conn)
-    id = json_response(conn, 201)["tenant"]["id"]
+    id = json_response(conn, 201)["id"]
 
     conn = delete(conn, "/api/tenants/#{id}")
     failure_conn = delete(conn, "/api/tenants/#{id}")
@@ -61,10 +61,10 @@ defmodule TenanteeWeb.TenantControllerTest do
 
   test "GET /api/tenants/:id/rents", %{conn: conn} do
     property_conn = insert_property(conn)
-    id = json_response(property_conn, 201)["property"]["id"]
+    id = json_response(property_conn, 201)["id"]
 
     tenant_conn = insert_tenant(conn)
-    tenant_id = json_response(tenant_conn, 201)["tenant"]["id"]
+    tenant_id = json_response(tenant_conn, 201)["id"]
 
     put(conn, "/api/properties/#{id}/tenants/#{tenant_id}")
     insert_rent(id, tenant_id)
@@ -79,10 +79,10 @@ defmodule TenanteeWeb.TenantControllerTest do
 
   test "GET /api/tenants/:id/rents/unpaid", %{conn: conn} do
     property_conn = insert_property(conn)
-    id = json_response(property_conn, 201)["property"]["id"]
+    id = json_response(property_conn, 201)["id"]
 
     tenant_conn = insert_tenant(conn)
-    tenant_id = json_response(tenant_conn, 201)["tenant"]["id"]
+    tenant_id = json_response(tenant_conn, 201)["id"]
 
     put(conn, "/api/properties/#{id}/tenants/#{tenant_id}")
     insert_rent(id, tenant_id)
