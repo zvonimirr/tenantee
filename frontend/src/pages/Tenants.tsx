092fd85be4184d7204dad679ac7e0450799d3084
Zvonimir Rudinski <zvonimirurdinski@protonmail.ch>
2022-11-27 14:07:34 +0100

    fix: fix eslint errors

--- frontend/src/pages/Tenants.tsx
+++ frontend/src/pages/Tenants.tsx
@@ -206,14 +206,14 @@ function Tenants() {
                             !isLoading &&
                             data &&
                             isEmpty(data.tenants) && (
-                                <GridItem colSpan={3}>
-                                    <Text fontSize="lg" textAlign="center">
-                                        {
-                                            "You don't have any tenants yet. Click the button below to add one."
-                                        }
-                                    </Text>
-                                </GridItem>
-                            )}
+                            <GridItem colSpan={3}>
+                                <Text fontSize="lg" textAlign="center">
+                                    {
+                                        'You don\'t have any tenants yet. Click the button below to add one.'
+                                    }
+                                </Text>
+                            </GridItem>
+                        )}
                     </Grid>
                     <Center>
                         <Button
