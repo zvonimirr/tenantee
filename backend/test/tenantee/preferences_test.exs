853aa09a969fa9aff42a5417e6b1ea55f8749cd2
Zvonimir Rudinski <zvonimirurdinski@protonmail.ch>
2022-11-28 00:20:46 +0100

    tests: fix preferences test

--- backend/test/tenantee/preferences_test.exs
+++ backend/test/tenantee/preferences_test.exs
@@ -3,20 +3,12 @@ defmodule Tenantee.PreferencesTest do
   alias Tenantee.Preferences
   alias Tenantee.Preferences.Schema
 
-  setup do
-    %Schema{}
-    |> Schema.changeset(%{name: "default_currency", value: "EUR"})
-    |> Repo.insert!()
-
-    :ok
-  end
-
   describe "get all preferences" do
     test "returns all preferences" do
       [default_currency] = Preferences.get_preferences()
 
       assert default_currency.name == :default_currency
-      assert default_currency.value == "EUR"
+      assert default_currency.value == "USD"
     end
   end
 
@@ -25,7 +17,7 @@ defmodule Tenantee.PreferencesTest do
       {:ok, preference} = Preferences.get_preference("default_currency")
 
       assert preference.name == :default_currency
-      assert preference.value == "EUR"
+      assert preference.value == "USD"
     end
 
     test "not found" do
@@ -35,11 +27,21 @@ defmodule Tenantee.PreferencesTest do
     end
   end
 
-  test "set preference" do
-    Preferences.set_preference("open_exchange_app_id", "123")
-    {:ok, preference} = Preferences.get_preference("open_exchange_app_id")
+  describe "set preference" do
+    test "insert" do
+      Preferences.set_preference("open_exchange_app_id", "123")
+      {:ok, preference} = Preferences.get_preference("open_exchange_app_id")
 
-    assert preference.name == :open_exchange_app_id
-    assert preference.value == "123"
+      assert preference.name == :open_exchange_app_id
+      assert preference.value == "123"
+    end
+
+    test "update" do
+      Preferences.set_preference("default_currency", "EUR")
+      {:ok, preference} = Preferences.get_preference("default_currency")
+
+      assert preference.name == :default_currency
+      assert preference.value == "EUR"
+    end
   end
 end
