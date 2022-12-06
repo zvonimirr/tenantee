40e25d18a59effa00079a55b0dbdeaebe24674d1
Zvonimir Rudinski <zvonimirurdinski@protonmail.ch>
2022-12-06 12:24:30 +0100

    feat: add dark mode

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,3 +1,4 @@
+import { ColorModeScript, theme } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
@@ -5,6 +6,7 @@ import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <App />
-    </React.StrictMode>
+    </React.StrictMode>,
 );
