bc626045f5c30b0dcc8e3a986b776736e341f943
Vince <vince.juric@noubis.com>
2022-12-26 13:32:28 +0100

    Added `rents` method to `TenantApiService`.

--- frontend/src/services/api/TenantApiService.ts
+++ frontend/src/services/api/TenantApiService.ts
@@ -1,8 +1,15 @@
 import { Tenant, TenantUpdateDto } from '../../types/tenant';
-import { ModelApiService } from './ModelApiService';
+import { HttpService } from './HttpService';
+import { ModelApiCallbackArgs, ModelApiService } from './ModelApiService';
+import { RentApiService } from './RentApiService';
 
 export class TenantApiService extends ModelApiService<Tenant, 'tenant', TenantUpdateDto> {
     public apiRoute = '/api/tenants';
+
+    async rents([url, ...rest]: ModelApiCallbackArgs): ReturnType<RentApiService['list']> {
+        const [id] = rest;
+        return HttpService.get(`${url}/${id}/rents`);
+    }
 }
 
 export const tenantApiService = new TenantApiService();
