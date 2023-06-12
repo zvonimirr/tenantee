72849a8de4a9d6f37f75cd0eb98a51de43f2e7cc
Zvonimir Rudinski <zvonimir.rudinski@noubis.com>
2023-06-12 07:19:13 +0200

    feat: update app version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Tenantee.MixProject do
   def project do
     [
       app: :tenantee,
-      version: "0.3.0",
+      version: "0.4.0",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
