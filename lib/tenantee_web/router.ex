d329b8c1ad4c12d28ae9e11d65924352fe309c80
Zvonimir Rudinski <zvonimir.rudinski@noubis.com>
2023-06-12 07:27:43 +0200

    fix: fix csp and ignore https (it's okay in prod)

--- lib/tenantee_web/router.ex
+++ lib/tenantee_web/router.ex
@@ -7,7 +7,8 @@ defmodule TenanteeWeb.Router do
     plug :fetch_live_flash
     plug :put_root_layout, {TenanteeWeb.Layouts, :root}
     plug :protect_from_forgery
-    plug :put_secure_browser_headers
+
+    plug :put_secure_browser_headers, %{"content-security-policy" => "default-src https: 'self'"}
   end
 
   pipeline :api do
--- mix.exs
+++ mix.exs
@@ -74,7 +74,8 @@ defmodule Tenantee.MixProject do
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
       "assets.setup": ["tailwind.install --if-missing", "esbuild.install --if-missing"],
       "assets.build": ["tailwind default", "esbuild default"],
-      "assets.deploy": ["tailwind default --minify", "esbuild default --minify", "phx.digest"]
+      "assets.deploy": ["tailwind default --minify", "esbuild default --minify", "phx.digest"],
+      sobelow: ["sobelow --config"]
     ]
   end
 end
