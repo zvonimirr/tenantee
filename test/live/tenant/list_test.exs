2db694d226ac062ec4b9e71277408d1413b35596
Zvonimir Rudinski <zvonimir.rudinski@noubis.com>
2023-06-19 03:32:19 +0200

    test: add manage rents test

--- test/live/tenant/list_test.exs
+++ test/live/tenant/list_test.exs
@@ -1,7 +1,8 @@
 defmodule TenanteeWeb.TenantListLiveTest do
   use TenanteeWeb.ConnCase
+  alias Tenantee.Entity.{Tenant, Rent}
   import Phoenix.LiveViewTest
-  import Tenantee.Test.Factory.Tenant
+  import Tenantee.Test.Factory.{Property, Tenant}
   import Tenantee.Test.Utils
   @endpoint TenanteeWeb.Endpoint
 
@@ -27,5 +28,20 @@ defmodule TenanteeWeb.TenantListLiveTest do
 
     assert view
            |> render_click("do_delete", %{id: tenant.id}) =~ "Tenant deleted successfully"
+  end
+
+  test "pays rent through modal", %{conn: conn} do
+    {:ok, tenant} = generate_tenant()
+    {:ok, property} = generate_property()
+    Tenant.add_to_property(tenant.id, property.id)
+    {:ok, rent} = Rent.create(tenant.id, property.id)
+
+    {:ok, view, _html} = live(conn, "/tenants")
+
+    assert view
+           |> render_click("manage_rents", %{id: tenant.id}) =~ "Manage rents"
+
+    assert view
+           |> render_click("pay_rent", %{rent: rent.id}) =~ "Rent paid successfully"
   end
 end
