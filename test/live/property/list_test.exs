2c044d252d479150585582a60b4814ac018059f2
Zvonimir Rudinski <zvonimir.rudinski@noubis.com>
2023-06-19 03:12:05 +0200

    test: add manage tenants test

--- test/live/property/list_test.exs
+++ test/live/property/list_test.exs
@@ -1,7 +1,7 @@
 defmodule TenanteeWeb.PropertyListLiveTest do
   use TenanteeWeb.ConnCase
   import Phoenix.LiveViewTest
-  import Tenantee.Test.Factory.Property
+  import Tenantee.Test.Factory.{Property, Tenant}
   import Tenantee.Test.Utils
   @endpoint TenanteeWeb.Endpoint
 
@@ -28,5 +28,32 @@ defmodule TenanteeWeb.PropertyListLiveTest do
 
     assert view
            |> render_click("do_delete", %{id: property.id}) =~ "Property deleted successfully"
+  end
+
+  test "opens lease modal", %{conn: conn} do
+    {:ok, _property} = generate_property()
+
+    {:ok, view, _html} = live(conn, "/properties")
+
+    assert view
+           |> element("button", "Manage tenants")
+           |> render_click() =~
+             "Toggling the checkbox will add or remove the tenant from the property."
+  end
+
+  test "toggles lease through modal", %{conn: conn} do
+    {:ok, property} = generate_property()
+    {:ok, tenant} = generate_tenant()
+
+    {:ok, view, _html} = live(conn, "/properties")
+
+    assert view
+           |> render_click("toggle_lease", %{property: property.id, tenant: tenant.id}) =~
+             "Lease updated successfully"
+
+    assert view
+           |> element("button", "Manage tenants")
+           |> render_click()
+           |> decode_html_entities() =~ "#{tenant.first_name} #{tenant.last_name}"
   end
 end
