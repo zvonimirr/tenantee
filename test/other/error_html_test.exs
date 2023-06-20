e2f97eb9d3c8eefa05e8019d204b1c8c8b9e60ca
Zvonimir Rudinski <zvonimir.rudinski@noubis.com>
2023-06-20 22:08:48 +0200

    test: add csp & errorhtml test

--- test/other/error_html_test.exs
+++ test/other/error_html_test.exs
@@ -1,8 +1,13 @@
 defmodule TenanteeWeb.ErrorHTMLTest do
+  alias TenanteeWeb.ErrorHTML
   use TenanteeWeb.ConnCase
 
   test "renders 404", %{conn: conn} do
     assert get(conn, "/404")
            |> html_response(404) =~ "Uh, oh"
+  end
+
+  test "renders unknown template", %{conn: conn} do
+    assert ErrorHTML.render("501.html", assigns: [conn: conn]) =~ "Not Implemented"
   end
 end
