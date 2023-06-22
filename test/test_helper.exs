6968a74186030e5b590c20c5c4fd8b19eaf5368b
Zvonimir Rudinski <zvonimir.rudinski@noubis.com>
2023-06-22 23:51:31 +0200

    fix: set tax in tests

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -8,5 +8,9 @@ if is_nil(Tenantee.Config.get(:name, nil)) do
   Tenantee.Config.set(:name, "Tenantee")
 end
 
+if is_nil(Tenantee.Config.get(:tax, nil)) do
+  Tenantee.Config.set(:tax, 0.1)
+end
+
 ExUnit.start()
 Ecto.Adapters.SQL.Sandbox.mode(Tenantee.Repo, :manual)
