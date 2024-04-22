41846a77ae1b964cbcee6363a3042be7f3b1217f
Zvonimir Rudinski <zvonimir.rudinski@noubis.com>
2024-04-22 06:06:47 +0200

    test(agreement): add agreement test

--- test/live/property/agreement_test.exs
+++ test/live/property/agreement_test.exs
@@ -1,4 +1,4 @@
-defmodule TenanteeWeb.PropertyAgreementAddTest do
+defmodule TenanteeWeb.PropertyAgreementTest do
   use TenanteeWeb.ConnCase
   import Phoenix.LiveViewTest
   import Tenantee.Test.Factory.Property
@@ -50,5 +50,54 @@ defmodule TenanteeWeb.PropertyAgreementAddTest do
       |> follow_redirect(conn)
 
     assert html =~ "agreement was created successfully"
+  end
+
+  test "edits an agreement", %{conn: conn} do
+    {:ok, tenant} = generate_tenant()
+    {:ok, property} = generate_property()
+    Tenant.add_to_property(tenant.id, property.id)
+    {:ok, view, _html} = live(conn, "/properties")
+
+    {:ok, view, _html} =
+      view
+      |> element("abbr[title='Generate Agreement'] a")
+      |> render_click()
+      |> follow_redirect(conn)
+
+    {:ok, view, _html} =
+      view
+      |> form("#agreement-form", %{
+        tenant_name: "None",
+        rent_amount: 1000,
+        lease_term: 12,
+        start_date: "2022-01-01",
+        end_date: "2022-12-31",
+        security_deposit: 1000,
+        additional_terms: "No pets allowed"
+      })
+      |> render_submit()
+      |> follow_redirect(conn)
+
+    {:ok, view, _html} =
+      view
+      |> element("a", "Edit Agreement")
+      |> render_click()
+      |> follow_redirect(conn)
+
+    {:ok, _view, html} =
+      view
+      |> form("#edit-agreement-form", %{
+        tenant_name: "None",
+        rent_amount: 1200,
+        lease_term: 24,
+        start_date: "2022-01-01",
+        end_date: "2023-12-31",
+        security_deposit: 1200,
+        additional_terms: "No pets allowed"
+      })
+      |> render_submit()
+      |> follow_redirect(conn)
+
+    assert html =~ "agreement was updated successfully"
   end
 end
